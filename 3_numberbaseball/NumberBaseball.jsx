efc3b1d7359f0b9d2cba4116e80de5efb4cd8f23
Bonggt <kepy1106@gmail.com>
2019-04-28 22:09:51 +0900

    NumberBaseball_module(import & require)

---
+++ 3_numberbaseball/NumberBaseball.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export const hello = "hello"; // import {hello} from "./NumberBaseball"
+
+class NumberBaseball extends Component {}
+
+export default NumberBaseball; // import NumberBaseball from "./NumberBaseball"
+
+// const React = require("react");
+// exports.hello = "hello";
+// module.exports = NumberBaseball;
