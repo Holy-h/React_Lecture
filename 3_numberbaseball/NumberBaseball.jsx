64b668b77e68fa9c44e0ebe27ace4cd34e9f1cde
Bonggt <kepy1106@gmail.com>
2019-04-29 23:23:59 +0900

    NumberBaseball_Loop(map)

--- 3_numberbaseball/NumberBaseball.jsx
+++ 3_numberbaseball/NumberBaseball.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
 
-export const hello = "hello"; // import {hello} from "./NumberBaseball"
+function getNumbers() {
+  // 겹치지 않는 숫자 4개를 랜덤하게 뽑는 함수
+}
 
-class NumberBaseball extends Component {}
+class NumberBaseball extends Component {
+  state = {
+    result: "",
+    value: "", //input
+    answer: getNumbers(),
+    tries: [] //시도 횟수
+  };
 
-export default NumberBaseball; // import NumberBaseball from "./NumberBaseball"
+  onSubmitForm = e => {
+    e.preventDefault();
+  };
 
-// const React = require("react");
-// exports.hello = "hello";
-// module.exports = NumberBaseball;
+  onChangeInput = e => {};
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            maxLength={4}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button>입력</button>
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          {["사과", "바나나", "배", "딸기", "양파", "포도"].map(a => {
+            return <li>{a}</li>;
+          })}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default NumberBaseball;
---
+++ 3_numberbaseball/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+
+const config = {
+  name: "numberbaseball-setting",
+  mode: "development", // 실서비스: production
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+
+  entry: {
+    app: "./client"
+  }, // 입력
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in KR"] // browserslist
+                },
+                debug: true
+              }
+            ],
+            ["@babel/preset-react"]
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-hot-loader/babel"
+          ]
+        }
+      }
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"), // __dirname(현재 폴더) + /dist
+    filename: "[name].js",
+    publicPath: "/dist/" // webpack-dev-server 설정(가상 경로 express.static)
+  } // 출력
+};
+
+module.exports = config;
