0191b3a6c896a13144104cb44fc3522e5042ef2e
Bonggt <kepy1106@gmail.com>
2019-05-08 20:48:11 +0900

    4-3 반응속도 체크(Class) 성능 체크

--- lecture/ResponseCheck.jsx
+++ lecture/ResponseCheck.jsx
@@ -4,7 +4,7 @@ class ResponseCheck extends Component {
   state = {
     state: "waiting", // waiting, ready, now
     message: "게임을 시작하려면 클릭하세요",
-    result: []
+    result: [],
   };
 
   // 변하지만 렌더링이 다시 일어나지 않아도 되는 아이들
@@ -17,12 +17,12 @@ class ResponseCheck extends Component {
     if (state === "waiting") {
       this.setState({
         state: "ready", // 빨간 화면
-        message: "초록색이 되면 클릭하세요"
+        message: "초록색이 되면 클릭하세요",
       });
       this.timeout = setTimeout(() => {
         this.setState({
           state: "now", // 초록 화면
-          message: "클릭하세요!"
+          message: "클릭하세요!",
         });
         this.startTime = new Date();
       }, Math.floor(Math.random() * 1000) + 2000); // 2초 ~ 3초 랜덤
@@ -33,7 +33,7 @@ class ResponseCheck extends Component {
       alert("부정 출발 노노!");
       this.setState({
         state: "waiting",
-        message: "초록색이 되면 클릭하세요"
+        message: "초록색이 되면 클릭하세요",
       });
     } else if (state === "now") {
       // 반응속도 체크
@@ -42,7 +42,7 @@ class ResponseCheck extends Component {
         return {
           state: "waiting",
           message: "게임을 시작하려면 클릭하세요",
-          result: [...prevState.result, this.endTime - this.startTime]
+          result: [...prevState.result, this.endTime - this.startTime],
         };
       });
       console.log(result);
@@ -51,7 +51,19 @@ class ResponseCheck extends Component {
 
   renderAverage = () => {
     const { result } = this.state;
-    return result.length < 1 ? null : <div>평균시간: {result.reduce((a, c) => a + c) / result.length}ms</div>;
+    return result.length < 1 ? null : (
+      <>
+        <div>평균시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+        <button onClick={this.onReset}>Reset</button>
+      </>
+    );
+  };
+
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+    alert("기록이 초기화되었습니다");
   };
 
   render() {
